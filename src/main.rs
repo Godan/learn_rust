a26a965a55d4384b2fa86b8f27aab9ffa2b3ae99
godan <shogo070920@gmail.com>
2021-01-11 13:55:39 +0900

    [add]

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
 extern crate rand;
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("Hello, world!");
     let secret_number = rand::thread_rng().gen_range(1,101);
-    println!("number is {}", secret_number);
+    
     println!("数あてゲーム");
-    let mut guess = String::new();
+    
 
-    io::stdin().read_line(&mut guess)
-        .expect("行の読み込みに失敗しました");
-    println!("予想: {}", guess);
+
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("行の読み込みに失敗しました");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+           
+        println!("予想: {}", guess);
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal =>{
+                println!("You win");
+                println!("number is {}", secret_number);
+                break;
+            } 
+        }
+    }
 }
